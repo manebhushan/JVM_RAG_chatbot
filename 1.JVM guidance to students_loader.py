60871fc77ea50f0a1d90ea8155445a771d0b6fb3
Bhushan <manebhushan@gmail.com>
2026-03-22 23:10:43 +0530

    merge 2 commit

--- 1.JVM guidance to students_loader.py
+++ 1.JVM guidance to students_loader.py
@@ -20,7 +20,7 @@ loader = PyMuPDFLoader("Jeevanvidyas-guidance-to-students.pdf")
 print("Splitting the document...")
 splitter = RecursiveCharacterTextSplitter(chunk_size = 1200,chunk_overlap = 100)
 embeddings = OllamaEmbeddings(model="sam860/granite-embedding-english:125m-Q8_0")
-
+print("Embedding creation complete!!!")
 docs = loader.load()
 # print(type(docs))
 # print(docs[10:11])
